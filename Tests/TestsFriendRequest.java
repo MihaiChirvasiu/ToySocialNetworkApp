766be43d6feb651ab3ee28484a71aa8693f1b139
Mihai Chirvasiu <mihaichirvasiu3@gmail.com>
2021-11-17 14:30:17 +0200

    Fixed TestsFriendRequest

--- Tests/TestsFriendRequest.java
+++ Tests/TestsFriendRequest.java
@@ -12,12 +12,12 @@ public class TestsFriendRequest {
         User user2 = new User("Ana", "Popica");
         user2.setId(2L);
         FriendRequest friendRequest = new FriendRequest(user1, user2);
-        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.PENDING));
+        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.pending));
         Assertions.assertTrue(friendRequest.getUser1().equals(user1));
         Assertions.assertTrue(friendRequest.getUser2().equals(user2));
         friendRequest.acceptRequest();
-        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.APPROVED));
+        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.approved));
         friendRequest.rejectRequest();
-        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.REJECTED));
+        Assertions.assertTrue(friendRequest.getStatus().equals(STATUS.rejected));
     }
 }
