f106c0c946afb7f6b5c1086a8410ae94030e0c30
Buboi Oana Andreea <oanabuboi@yahoo.com>
2021-11-17 10:29:22 +0200

    Added FriendRequest class

---
+++ src/com/company/Domain/FriendRequest.java
@@ -0,0 +1,39 @@
+package com.company.Domain;
+
+import com.company.Utils.STATUS;
+
+public class FriendRequest extends Entity<Long> {
+
+    private User user1;
+    private User user2;
+    private STATUS status;
+
+    public FriendRequest(User user1, User user2) {
+        this.user1 = user1;
+        this.user2 = user2;
+        this.status = STATUS.PENDING;
+    }
+
+    public User getIdUser1()
+    {
+        return user1;
+    }
+
+    public User getIdUser2() {
+        return user2;
+    }
+
+    public STATUS getStatus() {
+        return status;
+    }
+
+    public void acceptRequest()
+    {
+        this.status=STATUS.APPROVED;
+    }
+
+    public void rejectRequest()
+    {
+        this.status=STATUS.REJECTED;
+    }
+}
